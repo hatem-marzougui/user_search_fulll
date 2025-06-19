d988ad3f973b439cbccfd674bb69a00e5e9e95f3
hatem-marzougui <hatem.adresse@gmail.com>
2025-06-19 10:56:57 +0200

    [front] declare texts in constants

---
+++ src/Constants.ts
@@ -0,0 +1,17 @@
+export const HEADER_TITLE: string = "Github Search";
+export const VIEW_PROFILE_BUTTON_TEXT: string = "View Profile";
+export const LOGIN: string = "Login";
+export const EDIT_MODE: string = "Edit Mode";
+export const DUPLICATE_USERS: string = "Duplicate Users";
+export const SEARCH_PLACEHOLDER: string = "Rechercher...";
+export const USERS_API_ENDPOINT: string = "/search/users";
+export const RATE_LIMIT_ERROR_MESSAGE: string =
+  "Limite de requêtes GitHub atteinte. Réessaie dans une minute.";
+export const EMPTY_SEARCH_QUERY_ERROR: string = "Please enter a search query";
+export const NO_RESULTS_FOUND: string = "No result found  ";
+export const USER_CARD_AVATAR_ALT: string = "User Avatar";
+export const USER_CARD_ID_LABEL: string = "ID:";
+export const USER_CARD_LOGIN_LABEL: string = "Login:";
+export const USER_CARD_VIEW_PROFILE_BUTTON_TEXT: string = "View Profile";
+export const RATE_LIMIT: string = "rate limit";
+export const ELEMENT_SELECTED: string = "elements selected";
